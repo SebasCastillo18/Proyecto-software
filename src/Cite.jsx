96e65392c2a907a54ee27f447b5f9b00e289cc5c
sebastian castillo <sebascasti2019@gmail.com>
2023-09-01 11:35:21 -0500

    add cite

---
+++ src/Cite.jsx
@@ -0,0 +1,18 @@
+const Cite = () => {
+  return (
+    <main>
+      <h2>CAPITULO III</h2>
+      <h3>INSCRIPCIÓN SELECCIÓN Y ADMISIÓN</h3>
+
+      <h4>ARTICULO 8 - inscripción</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, non
+        officiis rem quidem nobis odit nesciunt eum laboriosam, rerum modi
+        reprehenderit consectetur? Eveniet unde voluptatem, facere dolor
+        similique quaerat incidunt.
+      </p>
+    </main>
+  );
+};
+
+export default Cite;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './Header.jsx'
-import './index.css'
+import './styles/index.css'
+import Cite from './Cite.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <Cite/>
   </React.StrictMode>,
 )
