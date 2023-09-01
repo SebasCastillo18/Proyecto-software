a5bd91bd86a6511e67587de4301159dfc4788fc3
sebastian castillo <sebascasti2019@gmail.com>
2023-09-01 09:20:12 -0500

    add new text

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
     <>
       <h1>Reglamento Estudiantil</h1>
 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus impedit, quis et optio porro repellendus qui, ut fugit delectus quia cum! Iste odio blanditiis doloremque? Facere reiciendis dolorum eligendi molestias.</p>
+
+
       
 
 
