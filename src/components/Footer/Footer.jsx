68583c0a81a8f0fc54c3327271f3213e3425bde2
Miguel Garavito <mgaravitocamargo@gmail.com>
2023-09-01 11:45:40 -0500

    sprint 1

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <a className="udec-link" href="http://www.ucundinamarca.edu.co/" target="blank">Universidad de Cundinamarca </a>
+        Vigilada MinEducación.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
