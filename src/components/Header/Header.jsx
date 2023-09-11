e9e876bf68974ea8ff678b873dbda2170f75b55d
sebastian castillo <sebascasti2019@gmail.com>
2023-09-11 14:35:49 -0500

    add logo dark mode

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,6 +11,7 @@ const Header = () => {
          <img className="img-logo" src="/public/images/ESCUDO-NEGRO-H.png" alt="" />
         <h1>Reglamento Estudiantil</h1>
         <img className="img-logo" src="/public/images/ESCUDO-NEGRO-H.png" alt="" />
+        <i className='bx bxs-moon dark-mode'></i>
     </header>
     
   )
