0a1acafd3790f59eb4144ed243aa46ca485c8874
Miguel Garavito <mgaravitocamargo@gmail.com>
2023-09-11 20:13:23 -0500

    json

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,9 +8,9 @@ const Header = () => {
 
     <header className="container-header">
      
-         <img className="img-logo" src="/public/images/ESCUDO-NEGRO-H.png" alt="" />
+         <img className="img-logo" src="/images/ESCUDO-NEGRO-H.png" alt="" />
         <h1>Reglamento Estudiantil</h1>
-        <img className="img-logo" src="/public/images/ESCUDO-NEGRO-H.png" alt="" />
+        <img className="img-logo" src="/images/ESCUDO-NEGRO-H.png" alt="" />
         <i className='bx bxs-moon dark-mode'></i>
     </header>
     
