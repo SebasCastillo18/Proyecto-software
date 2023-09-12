817ff0d1b09ded0d5e1103157e750beb08703d2e
sebastian castillo <sebascasti2019@gmail.com>
2023-09-11 19:38:02 -0500

    add styles dark-mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import Footer from './components/Footer/Footer.jsx'
 import Cite from './components/Cite/Cite.jsx'
 import Header from './components/Header/Header.jsx'
 
+
+
+
 const App = () => {
+
+
+
   return (
     <section className='App'>
         <Header/>
