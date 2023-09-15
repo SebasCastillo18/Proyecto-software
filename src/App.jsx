ef0b7fcea6fb3e36a7d82a60fdbb70391cba2908
Miguel Garavito <mgaravitocamargo@gmail.com>
2023-09-14 21:01:24 -0500

    reading json

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import "./App.css"
 import Footer from './components/Footer/Footer.jsx'
 import Cite from './components/Cite/Cite.jsx'
 import Header from './components/Header/Header.jsx'
+import articles from "./assets/articles.json";
+import { useState } from "react"
+import { getRandomNumber } from "./utils/getRandom"
 
 
 
 
 const App = () => {
-
+  const getRandomArticle = () => articles[getRandomNumber(articles.length - 1)];
+  const [article, setArticle] = useState(getRandomArticle());
 
 
   return (
     <section className='App'>
         <Header/>
-        <Cite/>
+        <Cite currentArticle={article}/>
         <Footer/>
     </section>
   )
--- src/components/Cite/Cite.jsx
+++ src/components/Cite/Cite.jsx
@@ -1,20 +1,41 @@
-import './Cite.css'
+import "./Cite.css";
 
-
-const Cite = () => {
+const Cite = ({ currentArticle }) => {
   return (
     <main>
-      <h2>CAPITULO III</h2>
-      <h3>INSCRIPCIÓN SELECCIÓN Y ADMISIÓN</h3>
+      <section>
+        <h2>{currentArticle.chapter}</h2>
+        <h3>{currentArticle.chapter_name}</h3>
 
-      <h4>ARTICULO 8 - inscripción</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, non
-        officiis rem quidem nobis odit nesciunt eum laboriosam, rerum modi
-        reprehenderit consectetur? Eveniet unde voluptatem, facere dolor
-        similique quaerat incidunt alterado por Miguel de nuevo.
-      </p>
-      <i className='bx bx-chevrons-right next-button'></i>
+        <h4>
+          ARTICULO {currentArticle.number} - {currentArticle.title}
+        </h4>
+        <p>
+          {currentArticle.body}
+          {currentArticle.items.length && (
+            <ol>
+              {currentArticle.items.map((item) => (
+                <li key={item}>
+                  <p>{item}</p>
+                </li>
+              ))}
+            </ol>
+          )}
+          {currentArticle.paragraphs.length && (
+            <ul>
+              {currentArticle.paragraphs.map((paragraph) => (
+                <li key={paragraph.title}>
+                  <p>{paragraph.title}</p>
+                  <p>{paragraph.content}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </p>
+      </section>
+      <button>
+        <i className="bx bx-chevrons-right next-button"></i>
+      </button>
     </main>
   );
 };
