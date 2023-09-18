1ecb1468c7bcc3d6bd84baebe7768ac04cc84a7b
sebastian castillo <sebascasti2019@gmail.com>
2023-09-18 12:00:21 -0500

    add styles in cite responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,14 @@
-import "./Header.css"
-
+import "./Header.css";
 
 const Header = () => {
   return (
-
-
-
     <header className="container-header">
-     
-         <img className="img-logo" src="/images/ESCUDO-NEGRO-H.png" alt="" />
-        <h1>Reglamento Estudiantil</h1>
-        <img className="img-logo" src="/images/ESCUDO-NEGRO-H.png" alt="" />
-        <i className='bx bxs-moon dark-mode'></i>
+      <img className="img-logo" src="/images/ESCUDO-NEGRO-H.png" alt="" />
+      <h1>Reglamento Estudiantil</h1>
+      <img className="img-logo" src="/images/ESCUDO-NEGRO-H.png" alt="" />
+      <i className="bx bxs-moon dark-mode"></i>
     </header>
-    
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
