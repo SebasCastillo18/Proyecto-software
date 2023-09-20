a7e22fd5d40003602d4ff9d607a2df8bcd8f9004
sebastian castillo <sebascasti2019@gmail.com>
2023-09-19 19:32:55 -0500

    styles new header and footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,9 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="container-header">
-      <img className="img-logo" src="/images/ESCUDO-NEGRO-H.png" alt="" />
+      <img className="img-logo" src="/images/ESCUDO-BLANCO-H.png" alt="" />
       <h1>Reglamento Estudiantil</h1>
-      <img className="img-logo" src="/images/ESCUDO-NEGRO-H.png" alt="" />
+      <img className="img-logo" src="/images/ESCUDO-BLANCO-H.png" alt="" />
       <i className="bx bxs-moon dark-mode"></i>
     </header>
   );
