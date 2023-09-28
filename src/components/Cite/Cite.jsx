ec060b2740e84bcfdbad1dda845cf2f3ce633a6b
sebastian castillo <sebascasti2019@gmail.com>
2023-09-28 14:12:56 -0500

    add input text for article

--- src/components/Cite/Cite.jsx
+++ src/components/Cite/Cite.jsx
@@ -3,43 +3,49 @@ import "./Cite.css";
 const Cite = ({ currentArticle, changeArticle }) => {
   const handleClick = () => {
     changeArticle();
-  }
+  };
   return (
-    <main>
-      <section className="article_section">
-        <h2>{currentArticle.chapter}</h2>
-        <h3>{currentArticle.chapter_name}</h3>
+    <div>
+      <form action="">
+        <label htmlFor="articulo">Buscar Articulo</label>
+        <input type="text" />
+      </form>
+      <main>
+        <section className="article_section">
+          <h2>{currentArticle.chapter}</h2>
+          <h3>{currentArticle.chapter_name}</h3>
 
-        <h4>
-          ARTICULO {currentArticle.number} - {currentArticle.title}
-        </h4>
-        <p>
-          {currentArticle.body}
-          {currentArticle.items.length > 0 && (
-            <ol className="items_list">
-              {currentArticle.items.map((item) => (
-                <li key={item}>
-                  <p>{item}</p>
-                </li>
-              ))}
-            </ol>
-          )}
-          {currentArticle.paragraphs.length > 0 && (
-            <ul className="paragraphs_list">
-              {currentArticle.paragraphs.map((paragraph) => (
-                <li key={paragraph.title}>
-                  <p>{paragraph.title}</p>
-                  <p>{paragraph.content}</p>
-                </li>
-              ))}
-            </ul>
-          )}
-        </p>
-        <div onClick={handleClick}>
-          <i className="bx bx-chevrons-right next-button"></i>
-        </div>
-      </section>
-    </main>
+          <h4>
+            ARTICULO {currentArticle.number} - {currentArticle.title}
+          </h4>
+          <p>
+            {currentArticle.body}
+            {currentArticle.items.length > 0 && (
+              <ol className="items_list">
+                {currentArticle.items.map((item) => (
+                  <li key={item}>
+                    <p>{item}</p>
+                  </li>
+                ))}
+              </ol>
+            )}
+            {currentArticle.paragraphs.length > 0 && (
+              <ul className="paragraphs_list">
+                {currentArticle.paragraphs.map((paragraph) => (
+                  <li key={paragraph.title}>
+                    <p>{paragraph.title}</p>
+                    <p>{paragraph.content}</p>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </p>
+          <div onClick={handleClick}>
+            <i className="bx bx-chevrons-right next-button"></i>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 };
 
