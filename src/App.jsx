341a4c7c8e038027006e9cfc2e30919ac093739a
sebastian castillo <sebascasti2019@gmail.com>
2023-09-29 07:54:38 -0500

    article and body styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import "./App.css"
-import Footer from './components/Footer/Footer.jsx'
-import Cite from './components/Cite/Cite.jsx'
-import Header from './components/Header/Header.jsx'
+import "./App.css";
+import Footer from "./components/Footer/Footer.jsx";
+import Cite from "./components/Cite/Cite.jsx";
+import Header from "./components/Header/Header.jsx";
 import articles from "./assets/articles.json";
-import { useState } from "react"
-import { getRandomNumber } from "./utils/getRandom"
-
-
-
+import { useState } from "react";
+import { getRandomNumber } from "./utils/getRandom";
 
 const App = () => {
   const getRandomArticle = () => articles[getRandomNumber(articles.length - 1)];
   const [article, setArticle] = useState(getRandomArticle());
 
-
   return (
-    <section className='App'>
-        <Header/>
-        <Cite currentArticle={article} changeArticle={() => setArticle(getRandomArticle())}/>
-        <Footer/>
+    <section className="App">
+      <Header />
+      <Cite
+        currentArticle={article}
+        changeArticle={() => setArticle(getRandomArticle())}
+      />
+      <Footer />
     </section>
-  )
-}
-export default App
+  );
+};
+export default App;
