a7671a4a2f5181a84c280a29f6b8496574b2e2e3
sebastian castillo <sebascasti2019@gmail.com>
2023-10-21 15:03:50 -0500

    add font, delete text in the footer, delete dark mode icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,26 @@
 import "./Header.css";
 
-const Header = ({selected, switchInfo}) => {
+const Header = ({ selected, switchInfo }) => {
   let option = "";
   let title = "";
-  if(selected){
+  if (selected) {
     option = "Gratuidad";
-    title = "Reglamento Estudiantil"
-  }
-  else{
+    title = "Reglamento Estudiantil";
+  } else {
     option = "Reglamento";
-    title = "Gratuidad"
+    title = "Gratuidad";
   }
   return (
     <header className="container-header">
-      <img className="img-logo" src="/images/ESCUDO-BLANCO-H.png" alt="escudo-blanco" />
+      <img
+        className="img-logo"
+        src="/images/ESCUDO-BLANCO-H.png"
+        alt="escudo-blanco"
+      />
       <h1 className="header-title">{title}</h1>
       <div className="header-buttons">
-        <button className="switch-btn" onClick={() => switchInfo()}>{option}</button>
-        <button className="dark-mode">
-          <i className="bx bxs-moon"></i>
+        <button className="switch-btn" onClick={() => switchInfo()}>
+          {option}
         </button>
       </div>
     </header>
