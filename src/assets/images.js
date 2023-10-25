802a15b9750a7d49fd14bf9f5806ee2d0df92b3e
sebastian castillo <sebascasti2019@gmail.com>
2023-10-25 14:35:25 -0500

    add udec2

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,5 +1,5 @@
 import udec1 from "/images/udec.jpeg";
-import udec2 from "/images/udec2.jpeg";
+import udec2 from "/images/udec2.jpg";
 import udec3 from "/images/udec3.jpeg";
 import udec4 from "/images/udec4.jpeg";
 import udec5 from "/images/udec5.jpeg";
