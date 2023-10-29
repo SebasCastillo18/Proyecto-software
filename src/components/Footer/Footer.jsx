52b3fad914eb7ef137d01037ffd9c2d58ad6e0a3
Miguel Garavito <mgaravitocamargo@gmail.com>
2023-10-29 18:15:31 -0500

    fix search gratuidad and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,15 @@
 import "./Footer.css"
 
-const Footer = () => {
+const Footer = ({ isReglamento }) => {
   return (
     <footer>
       <a className="udec-link" href="http://www.ucundinamarca.edu.co/" target="blank">Universidad de Cundinamarca</a>
      
-      <a className="rule" target="_blan" href="https://www.ucundinamarca.edu.co/index.php/reglamento-estudiantil">Reglamento</a>
+     {isReglamento ? (
+        <a className="rule" target="_blan" href="https://www.ucundinamarca.edu.co/index.php/reglamento-estudiantil">Reglamento</a>
+      ) : (
+        <a className="rule" target="_blan" href="https://www.mineducacion.gov.co/portal/Educacion-superior/Pol%C3%ADtica-de-Gratuidad/409830:Politica-de-Gratuidad-en-la-Educacion-Superior#:~:text=La%20Pol%C3%ADtica%20de%20Gratuidad%20es,y%20que%20cumplen%20los%20requisitos">Gratuidad</a>
+      )}
     </footer>
   )
 }
